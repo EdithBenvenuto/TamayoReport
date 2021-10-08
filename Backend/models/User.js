efa151b543a08dc8dd85c4743cd0e3e00b11ea55
edithbenvenuto <edithpau_benvenuto@hotmail.com>
2021-10-08 13:15:09 -0500

    Modelo usuario
    Modelo de usuario y user services comenzado

---
+++ Backend/models/User.js
@@ -0,0 +1,45 @@
+const { isEmail } = require('validator');
+const bcrypt = require("bcryptjs");
+const mongoose = require('mongoose');
+
+const UsersSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [isEmail, 'Invalid email']
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+UsersSchema.pre("save", function (next) {
+    const user = this
+
+    if (this.isModified("password") || this.isNew) {
+        bcrypt.genSalt(10, function (saltError, salt) {
+            if (saltError) {
+                return next(saltError)
+            } else {
+                bcrypt.hash(user.password, salt, function (hashError, hash) {
+                    if (hashError) {
+                        return next(hashError)
+                    }
+
+                    user.password = hash
+                    next()
+                })
+            }
+        })
+    } else {
+        return next()
+    }
+});
+
+module.exports = mongoose.model('users', UsersSchema);
