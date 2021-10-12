c3299ab990ae6f013f8e42048c431590142340a5
Danielema2000 <a00227518@itesm.mx>
2021-10-12 10:55:07 -0500

    Commit my branch
    Agregue mis ventanas

---
+++ Frontend/app/src/main/java/com/example/tamayoreport/controller/VerReporte_Admin.kt
@@ -0,0 +1,12 @@
+package com.example.tamayoreport.controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tamayoreport.R
+
+class VerReporte_Admin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ver_reporte_admin)
+    }
+}
